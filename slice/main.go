25ee5fa2aabcb705ac772e7824c02e28baf8e952
muhammadakbaar <mohammadakbaar11@gmail.com>
2023-02-28 16:44:19 +0700

    update learn about map

--- slice/main.go
+++ slice/main.go
@@ -1,1 +1,11 @@
-package slice
+package main
+
+import "fmt"
+
+func main() {
+	var fruits = []string{"apple", " manggo", "banana", "papaya"}
+	var newFruits = fruits[0:2]
+
+	fmt.Println(newFruits)
+
+}
