9173560972f7ffff275ec709b6a452bf21dbc3ba
muhammadakbaar <mohammadakbaar11@gmail.com>
2023-03-24 14:35:25 +0700

    fungtional closure

--- fungsivariadic/main.go
+++ fungsivariadic/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	var avg = calculate(2, 4, 3, 5, 4, 3, 3, 5, 5, 3)
+	var numbers = []int{2, 4, 3, 5, 4, 3, 3, 5, 5, 3}
+	var avg = calculate(numbers...)
 	var msg = fmt.Sprintf("Rata-rata : %.2f", avg)
 	fmt.Println(msg)
+	yourHobbies("Wick", "Sleeping ", "eating")
+
 }
 
 func calculate(number ...int) float64 {
@@ -15,4 +21,10 @@ func calculate(number ...int) float64 {
 	}
 	var avg = float64(total) / float64(len(number))
 	return avg
+}
+func yourHobbies(name string, hobbies ...string) {
+	var hobbiesasAsString = strings.Join(hobbies, ",")
+
+	fmt.Printf("Hello my name is : %s\n", name)
+	fmt.Printf("My hobbies are : %s\n", hobbiesasAsString)
 }
