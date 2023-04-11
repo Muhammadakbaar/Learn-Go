397f0ce0f04ceeb80220440fe8cd8b4e820b0f5f
muhammadakbaar <mohammadakbaar11@gmail.com>
2023-04-11 22:23:20 +0700

    sungsi variadic v2

--- fungsi-variadic/main.go
+++ fungsi-variadic/main.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	total := sumAll(10, 10, 10, 10, 10)
 	fmt.Println(total)
+	slice := []int{20, 20, 20, 20, 20}
+	total = sumAll(slice...)
+	fmt.Println(total)
 }
 func sumAll(number ...int) int {
 	total := 0
