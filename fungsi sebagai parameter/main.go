cfe1cdb6fa3fb2befa2bf73e3ab99c97340d62d1
muhammadakbaar <mohammadakbaar11@gmail.com>
2023-04-16 04:08:30 +0700

    fungsi sebagai parameter

--- fungsi sebagai parameter/main.go
+++ fungsi sebagai parameter/main.go
@@ -1,1 +1,21 @@
 package main
+
+import "fmt"
+
+func sayHello(name string, filter func(string) string) {
+	namedfilter := filter(name)
+	fmt.Println("Hello", namedfilter)
+}
+
+func spamFilter(name string) string {
+	if name == "Bangsat" {
+		return "B***at"
+	} else {
+		return name
+	}
+}
+func main() {
+	sayHello("Akbar", spamFilter)
+	sayHello("Bangsat", spamFilter)
+
+}
