d0df460d68bbe5eadbd7d0fbf7fd528682011d4b
muhammadakbaar <mohammadakbaar11@gmail.com>
2023-04-15 22:10:50 +0700

    fungsi sebagai parameter

--- fungsi-value/main.go
+++ fungsi-value/main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
+func getName(name string) string {
+	return "nama gue" + name
+}
 func main() {
+
+	name := getName(" Muhammad")
+
+	fmt.Println(getName(" Akbar"))
+	fmt.Println(name)
 
 }
